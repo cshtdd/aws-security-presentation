0c03deac12c424bf5834a775bd647e8f04e4cd77
Camilo Sanchez <camilin87@gmail.com>
2020-01-13 08:39:11 -0500

    Create a Cloudwatch Event rule for the AWS Config rules change

---
+++ 20_config_automation_simple.tf
@@ -0,0 +1,17 @@
+resource "aws_cloudwatch_event_rule" "config_rule" {
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.config"
+  ],
+  "detail-type": [
+    "Config Rules Compliance Change"
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "config_to_sns" {
+  rule = aws_cloudwatch_event_rule.config_rule.name
+  arn = aws_sns_topic.alerts.arn
+}
