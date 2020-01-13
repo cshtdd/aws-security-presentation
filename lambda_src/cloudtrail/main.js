1ab8bcfdbd5a1cca33a7897f727d12dda24b51ed
Camilo Sanchez <camilin87@gmail.com>
2020-01-13 10:37:24 -0500

    Tweak the CloudTrail lambda code to trigger notifications for IAM events

--- lambda_src/cloudtrail/main.js
+++ lambda_src/cloudtrail/main.js
@@ -1,7 +1,20 @@
 'use strict';
 
 exports.handler = (event, context, callback) => {
-    console.log('CloudTrailEvent');
-    console.log('Received event:', JSON.stringify(event, null, 2));
-    callback(null, 'Success');
+    console.log(JSON.stringify(event, null, 2));
+
+    let eventDetails = event.detail;
+    if (eventDetails.readOnly){
+        callback(null, 'Ignore ReadOnly Event');
+        return;
+    }
+
+    if (eventDetails.eventSource !== "iam.amazonaws.com"){
+        callback(null, 'Ignore non IAM Event');
+        return;
+    }
+
+    // TODO publish SNS Notification
+
+    callback(null, 'IAM Modification');
 };
