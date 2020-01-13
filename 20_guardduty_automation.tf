42ae42b597bbdbf45d056325112ed94e459b1798
Camilo Sanchez <camilin87@gmail.com>
2020-01-13 07:56:25 -0500

    Give the lambda the correct permissions

--- 20_guardduty_automation.tf
+++ 20_guardduty_automation.tf
@@ -5,6 +5,13 @@ resource "aws_sns_topic_subscription" "guardduty_announcements_lambda_subscripti
   endpoint_auto_confirms = true
 }
 
+resource "aws_lambda_permission" "guardduty_lambda_permissions" {
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.guardduty_lambda.function_name
+  principal = "sns.amazonaws.com"
+  source_arn = var.guardduty_announcements_topic_arn
+}
+
 data "archive_file" "guardduty_lambda_zip" {
   type = "zip"
   source_file = "${path.module}/lambda_src/guardduty/main.js"
@@ -42,4 +49,34 @@ resource "aws_iam_role" "guardduty_lambda_iam_role" {
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "guardduty_lambda_basic_execution_role_policy_attachment" {
+  role = aws_iam_role.guardduty_lambda_iam_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "guardduty_lambda_policy_attachment" {
+  role = aws_iam_role.guardduty_lambda_iam_role.id
+  policy_arn = aws_iam_policy.guardduty_lambda_policy.arn
+}
+
+resource "aws_iam_policy" "guardduty_lambda_policy" {
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "sns:Publish",
+      "Resource": "${aws_sns_topic.alerts.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_log_group" "guardduty_lambda_logs" {
+  name = "${aws_lambda_function.guardduty_lambda.function_name}-logs"
+  retention_in_days = 90
 }
