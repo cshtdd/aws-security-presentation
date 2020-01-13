d05765272ac44e98bba81f15fb3f7e92d8937276
Camilo Sanchez <camilin87@gmail.com>
2020-01-13 08:32:41 -0500

    Subscribe GuarDuty findings to the SNS Alerts topic with CloudWatch event rules

--- 00_sns_alerts.tf
+++ 00_sns_alerts.tf
@@ -2,6 +2,23 @@ resource "aws_sns_topic" "alerts" {
   name = "${var.shared_prefix}-alerts"
 }
 
+resource "aws_sns_topic_policy" "alerts_policy" {
+  arn = aws_sns_topic.alerts.arn
+  policy = data.aws_iam_policy_document.alerts_policy.json
+}
+
+data "aws_iam_policy_document" "alerts_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["SNS:Publish"]
+    resources = [aws_sns_topic.alerts.arn]
+    principals {
+      identifiers = ["events.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
 output "alerts_topic_arn" {
   value = aws_sns_topic.alerts.arn
 }
---
+++ 20_guardduty_automation_simple.tf
@@ -0,0 +1,14 @@
+resource "aws_cloudwatch_event_rule" "guardduty_rule" {
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.guardduty"
+  ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "guardduty_to_sns" {
+  rule = aws_cloudwatch_event_rule.guardduty_rule.name
+  arn = aws_sns_topic.alerts.arn
+}
