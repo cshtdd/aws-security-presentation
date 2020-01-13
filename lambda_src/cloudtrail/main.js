88902db76923065520703ed699e2afefa3cc1d79
Camilo Sanchez <camilin87@gmail.com>
2020-01-13 11:34:47 -0500

    Publish an SNS Notification whenever the Cloudtrail monitoring lambda gets invoked with an IAM event. However, the CloudWatch rule does not invoke the lambda for all the Cloudtrail events as you'd expect

--- lambda_src/cloudtrail/main.js
+++ lambda_src/cloudtrail/main.js
@@ -1,20 +1,36 @@
 'use strict';
+var AWS = require("aws-sdk");
 
 exports.handler = (event, context, callback) => {
-    console.log(JSON.stringify(event, null, 2));
+    let eventText = JSON.stringify(event, null, 2);
+    console.log(eventText);
 
     let eventDetails = event.detail;
     if (eventDetails.readOnly){
+        console.log('Ignore ReadOnly Event');
         callback(null, 'Ignore ReadOnly Event');
         return;
     }
 
     if (eventDetails.eventSource !== "iam.amazonaws.com"){
+        console.log('Ignore non IAM Event');
         callback(null, 'Ignore non IAM Event');
         return;
     }
 
-    // TODO publish SNS Notification
+    console.log('Publish SNS Notification');
+    let snsAlertsTopicArn = process.env.ALERTS_SNS_TOPIC_ARN;
 
-    callback(null, 'IAM Modification');
+    let sns = new AWS.SNS();
+    let params = {
+        Message: eventText,
+        TopicArn: snsAlertsTopicArn
+    };
+    sns.publish(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        console.log('SNS Publish Completed');
+        callback(null, 'IAM Modification');
+    });
 };
