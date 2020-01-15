fe5ea79d6a76a52ffb90b53ff5cb396cfacd7be8
Camilo Sanchez <camilin87@gmail.com>
2020-01-15 09:04:07 -0500

    Create the athena table but it doesn't work

--- 20_cloudtrail_automation_athena.tf
+++ 20_cloudtrail_automation_athena.tf
@@ -19,27 +19,85 @@ output "s3_bucket_athena_results" {
   value = aws_s3_bucket.bucket_athena_results.id
 }
 
+locals {
+  athena_query_results_location = "s3://${aws_s3_bucket.bucket_athena_results.id}/cw/"
+}
+
 resource "aws_athena_workgroup" "compliance" {
   name = "${var.shared_prefix}_compliance"
 
   configuration {
     result_configuration {
-      output_location = "s3://${aws_s3_bucket.bucket_athena_results.id}/cw/"
+      output_location = local.athena_query_results_location
     }
   }
 }
 
 locals {
-  athena_table_creation_script = ""
+  athena_table_creation_script = <<EOF
+CREATE EXTERNAL TABLE IF NOT EXISTS cloudtrail_logs (
+eventversion STRING,
+useridentity STRUCT<
+               type:STRING,
+               principalid:STRING,
+               arn:STRING,
+               accountid:STRING,
+               invokedby:STRING,
+               accesskeyid:STRING,
+               userName:STRING,
+sessioncontext:STRUCT<
+attributes:STRUCT<
+               mfaauthenticated:STRING,
+               creationdate:STRING>,
+sessionissuer:STRUCT<
+               type:STRING,
+               principalId:STRING,
+               arn:STRING,
+               accountId:STRING,
+               userName:STRING>>>,
+eventtime STRING,
+eventsource STRING,
+eventname STRING,
+awsregion STRING,
+sourceipaddress STRING,
+useragent STRING,
+errorcode STRING,
+errormessage STRING,
+requestparameters STRING,
+responseelements STRING,
+additionaleventdata STRING,
+requestid STRING,
+eventid STRING,
+resources ARRAY<STRUCT<
+               ARN:STRING,
+               accountId:STRING,
+               type:STRING>>,
+eventtype STRING,
+apiversion STRING,
+readonly STRING,
+recipientaccountid STRING,
+serviceeventdetails STRING,
+sharedeventid STRING,
+vpcendpointid STRING
+)
+ROW FORMAT SERDE 'com.amazon.emr.hive.serde.CloudTrailSerde'
+STORED AS INPUTFORMAT 'com.amazon.emr.cloudtrail.CloudTrailInputFormat'
+OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://${aws_s3_bucket.compliance_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/';
+EOF
+}
+
+locals {
+  athena_table_creation_cmd = "aws athena start-query-execution --query-string \"${local.athena_table_creation_script}\" --query-execution-context \"Database=${aws_athena_database.compliance_db.name}\" --work-group \"${aws_athena_workgroup.compliance.name}\" --result-configuration \"OutputLocation=${local.athena_query_results_location}\""
 }
 
 resource "null_resource" "athena_table_creation" {
   triggers = {
-    table_creation_hash = md5(local.athena_table_creation_script)
+    table_creation_hash = md5(local.athena_table_creation_cmd)
   }
 
   provisioner "local-exec" {
-    command =  "aws iam get-user"
+    command = local.athena_table_creation_cmd
   }
 
   depends_on = [aws_athena_workgroup.compliance]
