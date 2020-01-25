2c7be86bcf43737db72fed3262ff1b0eca2a9e2d
Camilo Sanchez <camilin87@gmail.com>
2020-01-25 10:33:25 -0500

    Publish SNS notifications with the contents of the cloudtrail csv

--- lambda_src/s3/main.js
+++ lambda_src/s3/main.js
@@ -9,6 +9,19 @@ function getObject(s3, info) {
     });
 }
 
+function publishSnsMessage(sns, topicArn, messageText) {
+    return new Promise((resolve, reject) => {
+        let params = {
+            Message: messageText,
+            TopicArn: topicArn
+        };
+        sns.publish(params, function (err, data) {
+            if (err)  return reject(err);
+            resolve(data);
+        });
+    });
+}
+
 exports.handler = function(event, context, callback) {
     console.log('S3 Lambda Invocation');
     console.log(JSON.stringify(event));
@@ -34,18 +47,29 @@ exports.handler = function(event, context, callback) {
     var processedCount = 0;
 
     var s3 = new AWS.S3();
+    let sns = new AWS.SNS();
+    let snsAlertsTopicArn = process.env.ALERTS_SNS_TOPIC_ARN;
 
     csvFiles.forEach((csvInfo) => {
         console.log('Downloading', csvInfo);
 
         getObject(s3, csvInfo)
             .then((data) => {
-                // console.log(JSON.stringify(data));
-
                 var csvContents = data.Body.toString();
-                console.log(csvContents);
+                console.log('Publishing', csvContents);
 
+                publishSnsMessage(sns, snsAlertsTopicArn, csvContents)
+                    .then((result) => {
+                        console.log('Publish result', JSON.stringify(result));
+
+                        processedCount++;
+
+                        if (processedCount === csvFiles.length){
+                            console.log('Completed Successfully');
                 callback(null, 'DONE');
+                        }
+                    })
+                    .catch(abortWithError);
             })
             .catch(abortWithError);
     });
