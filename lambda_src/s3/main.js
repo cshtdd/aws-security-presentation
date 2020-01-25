642e96cd84ca5d9c1ab93ffa95928d32d6e971aa
Camilo Sanchez <camilin87@gmail.com>
2020-01-25 10:01:09 -0500

    Log the information of the files

--- lambda_src/s3/main.js
+++ lambda_src/s3/main.js
@@ -4,10 +4,21 @@ exports.handler = function(event, context, callback) {
     console.log('S3 Lambda Invocation');
     console.log(JSON.stringify(event));
 
-    if (event.eventName !== 'ObjectCreated:Put'){
-        callback(null, 'Ignore');
-        return;
-    }
+    // event.Records.forEach(er => console.log(er.s3));
+
+    var csvFiles = event.Records
+        .filter(r => r.eventName === 'ObjectCreated:Put')
+        .map(r => r.s3)
+        .map(b => {
+            return {
+                bucket: b.bucket.name,
+                key: b.object.key
+            }
+        });
+
+    csvFiles.forEach((csvInfo) => {
+        console.log(csvInfo);
+    });
 
     callback(null, 'DONE');
 };
