b74868e12dede0cc5e1fe1a4966a80a5db7f79d0
Camilo Sanchez <camilin87@gmail.com>
2020-01-25 10:13:00 -0500

    Download the created csv files with the results

--- lambda_src/s3/main.js
+++ lambda_src/s3/main.js
@@ -1,24 +1,48 @@
 const AWS = require('aws-sdk');
 
+function getObject(s3, info) {
+    return new Promise((resolve, reject) => {
+        s3.getObject(info, function(err, data) {
+            if (err) return reject(err);
+            resolve(data);
+        });
+    });
+}
+
 exports.handler = function(event, context, callback) {
     console.log('S3 Lambda Invocation');
     console.log(JSON.stringify(event));
 
-    // event.Records.forEach(er => console.log(er.s3));
+    function abortWithError(e) {
+        console.log('ERROR: ', e);
+        callback(null, 'ERROR');
+    }
 
     var csvFiles = event.Records
         .filter(r => r.eventName === 'ObjectCreated:Put')
         .map(r => r.s3)
         .map(b => {
             return {
-                bucket: b.bucket.name,
-                key: b.object.key
+                Bucket: b.bucket.name,
+                Key: b.object.key
             }
         });
 
-    csvFiles.forEach((csvInfo) => {
-        console.log(csvInfo);
-    });
+    var processedCount = 0;
 
-    callback(null, 'DONE');
+    var s3 = new AWS.S3();
+
+    csvFiles.forEach((csvInfo) => {
+        console.log('Downloading', csvInfo);
+
+        getObject(s3, csvInfo)
+            .then((data) => {
+                console.log(JSON.stringify(data));
+
+                // data.Body
+
+                callback(null, 'DONE');
+            })
+            .catch(abortWithError);
+    });
 };
