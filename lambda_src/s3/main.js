08cca3843e422d052b75f916876bbfd730a00a80
Camilo Sanchez <camilin87@gmail.com>
2020-01-25 09:52:07 -0500

    Log the request info in the lambda subscribed to S3 events

--- lambda_src/s3/main.js
+++ lambda_src/s3/main.js
@@ -1,5 +1,13 @@
 const AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
+    console.log('S3 Lambda Invocation');
+    console.log(JSON.stringify(event));
+
+    if (event.eventName !== 'ObjectCreated:Put'){
+        callback(null, 'Ignore');
+        return;
+    }
+
     callback(null, 'DONE');
 };
